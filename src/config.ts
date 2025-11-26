fea5a53fa6a57e34fe8473f0200b7f91fa782612
reinamontage <reinamontage@gmail.com>
2025-11-26 21:49:28 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Mon Portfolio💛';
+export const SITE_DESCRIPTION = 'Je suis Reina. J’accompagne des créateurs YouTube, coachs et entrepreneurs qui veulent une image premium, plus de crédibilité et surtout du temps libéré. Mon travail, c’est de transformer vos vidéos en contenu pro, cohérent, qui vous positionne au-dessus de la concurrence et qui vous aide à vendre plus.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true

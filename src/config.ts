6b6a4e56717319fea109b32e5501b56d94bdbc7c
reinamontage <reinamontage@gmail.com>
2025-11-26 21:57:42 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Mon Portfolio💛';
-export const SITE_DESCRIPTION = 'Je transforme vos vidéos en contenu pro et cohérent, pour plus de crédibilité et de ventes.';
+export const SITE_DESCRIPTION = 'Je suis Reina. J’accompagne des créateurs YouTube, coachs et entrepreneurs qui veulent une image premium, plus de crédibilité et surtout du temps libéré. Mon travail, c’est de transformer vos vidéos en contenu pro, cohérent, qui vous positionne au-dessus de la concurrence et qui vous aide à vendre plus.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
